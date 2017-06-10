b48f30a62b32d709c617ba42d096b18dcf959a2c
Piotr Maks <piotr.maks@nokia.com>
2017-06-11 00:09:52 +0200

    move to azure

--- TeamViewer/Models/TeamViewerContext.cs
+++ TeamViewer/Models/TeamViewerContext.cs
@@ -15,7 +15,7 @@ namespace TeamViewer.Models
         // For more information refer to the documentation:
         // http://msdn.microsoft.com/en-us/data/jj591621.aspx
     
-        public TeamViewerContext() : base("name=TeamViewerContext")
+        public TeamViewerContext() : base("name=TeamViewerAzure")
         {
             this.Database.Log = s => System.Diagnostics.Debug.WriteLine(s);
         }
