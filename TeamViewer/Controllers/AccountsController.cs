199ba32ec66a3a276aed95ed1acdb7726faf8fef
Radoslaw Kochman <garzum@tlen.pl>
2017-06-10 20:57:16 +0200

    +AccountsController

---
+++ TeamViewer/Controllers/AccountsController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace TeamViewer.Controllers
+{
+    [System.Web.Http.RoutePrefix("api/accounts")]
+    public class AccountsController : BaseApiController
+    {
+
+        [System.Web.Http.Route("users")]
+        public IHttpActionResult GetUsers()
+        {
+            return Ok(this.AppUserManager.Users.ToList().Select(u => this.TheModelFactory.Create(u)));
+        }
+
+        [System.Web.Http.Route("user/{id:guid}", Name = "GetUserById")]
+        public async Task<IHttpActionResult> GetUser(string Id)
+        {
+            var user = await this.AppUserManager.FindByIdAsync(Id);
+
+            if (user != null)
+            {
+                return Ok(this.TheModelFactory.Create(user));
+            }
+
+            return NotFound();
+
+        }
+
+        [System.Web.Http.Route("user/{username}")]
+        public async Task<IHttpActionResult> GetUserByName(string username)
+        {
+            var user = await this.AppUserManager.FindByNameAsync(username);
+
+            if (user != null)
+            {
+                return Ok(this.TheModelFactory.Create(user));
+            }
+
+            return NotFound();
+
+        }
+    }
+}
--- TeamViewer/Infrastructure/ApplicationUserManager.cs
+++ TeamViewer/Infrastructure/ApplicationUserManager.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
 using System;
 using System.Collections.Generic;
 using System.Linq;
