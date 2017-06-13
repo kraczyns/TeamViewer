1e917f1d835f2d8ffc6e9b86802180546c9675fd
Radoslaw Kochman <garzum@tlen.pl>
2017-06-13 18:36:00 +0200

    Poprawiony error z Link() w ModelFactory

--- TeamViewer/Models/ModelFactory.cs
+++ TeamViewer/Models/ModelFactory.cs
@@ -1,19 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net.Http;
+//using System.Net.Http;
 using System.Web;
 using System.Web.Mvc;
 using TeamViewer.Infrastructure;
+using System.Web.Http;
+using System.Web.Http.Routing;
 
 namespace TeamViewer.Models
 {
     public class ModelFactory
     {
-        private UrlHelper _UrlHelper;
+        private System.Web.Http.Routing.UrlHelper _UrlHelper;
         private ApplicationUserManager _AppUserManager;
 
-        public ModelFactory(HttpRequestMessage request, ApplicationUserManager appUserManager)
+        public ModelFactory(System.Net.Http.HttpRequestMessage request, ApplicationUserManager appUserManager)
         {
             _UrlHelper = new UrlHelper(request);
             _AppUserManager = appUserManager;
